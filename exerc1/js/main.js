55394e32e043963c63e9a24eb75f122362cd24cf
Baracho <baracholss@gmail.com>
2021-07-14 19:49:52 -0300

    Exercicio1Variaveis

---
+++ exerc1/js/main.js
@@ -0,0 +1,7 @@
+var nome = "Leandro";
+var n1 = 34;
+var n2 = 38;
+var frase = "Japão é o melhor do mundo";
+console.log(frase.replace("Japão", "Brasil"));
+console.log(n1 + n2);
+alert(frase.replace("Japão", "Brasil"));
